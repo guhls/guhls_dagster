5900604de041b08b854563760bd593164fe02e65
Gustavo Silva <guhenriquels@gmail.com>
2022-10-11 18:42:48 -0300

    Created simple_etl DAG with airflow

---
+++ airflow_dags/dags/simple_dag.py
@@ -0,0 +1,18 @@
+from airflow.models import DAG
+from airflow.operators.python import PythonOperator
+from dags_airflow import etl_pipe
+import datetime
+
+dag = DAG(
+    dag_id="etl_pipe",
+    schedule="0 * * * *",
+    start_date=datetime.datetime(2022, 10, 11)
+)
+
+simple_etl = PythonOperator(
+    task_id="simple_etl",
+    python_callable=etl_pipe,
+    dag=dag,
+)
+
+simple_etl.set_upstream(etl_pipe())
