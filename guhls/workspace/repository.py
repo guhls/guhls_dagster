dc4f76a8db8554951a7819f45c5dc8219f753fa5
Gustavo Silva <guhenriquels@gmail.com>
2022-10-30 14:47:08 -0300

    Added elections_job and schedule in repository

--- guhls/workspace/repository.py
+++ guhls/workspace/repository.py
@@ -1,6 +1,7 @@
 from dagster import repository
 from guhls.NSSDCA.pipelines import nssdca_pipe
 from guhls.yelp.pipelines import yelp_data_pipe
+from guhls.elections.job import elections_job, schedule
 
 
 @repository
@@ -8,4 +9,6 @@ def guhls_repository():
     return [
         nssdca_pipe,
         yelp_data_pipe,
+        elections_job,
+        schedule,
     ]
