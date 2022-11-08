f0c8ea3e413aeb13da5f2ee3a05da2e97614f5c8
Gustavo Silva <guhenriquels@gmail.com>
2022-11-08 19:52:02 -0300

    Created s3_resources

---
+++ guhls/resources/s3.py
@@ -0,0 +1,18 @@
+from dagster import resource
+import boto3
+
+
+class S3ResourceConstructor:
+    def __init__(self):
+        self.client_s3 = boto3.client('s3')
+
+    def upload_file(self, file, key):
+        self.client_s3.upload_fileobj(Bucket="guhls-dagster", Fileobj=file, Key=key)
+
+
+@resource()
+def s3_resource():
+    try:
+        return S3ResourceConstructor
+    except Exception:
+        raise "Check Credentials in AWS"
